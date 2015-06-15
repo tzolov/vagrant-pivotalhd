cb5b602dc2017efbb9513a5d1e89ad970d8ceadc
tzolov <christian.tzolov@gmail.com>
2015-06-15 14:21:18 +0200

    increase the sleep period before deploying the cluster

--- scripts/start_cluster.sh
+++ scripts/start_cluster.sh
@@ -5,5 +5,5 @@ curl --user admin:admin -H 'X-Requested-By:ambari' -X POST http://ambari.localdo
 
 cp /vagrant/blueprints/$1-hostmapping.json .
 cat $1-hostmapping.json
-sleep 5
+sleep 15
 curl --user admin:admin -H 'X-Requested-By:ambari' -X POST http://ambari.localdomain:8080/api/v1/clusters/phd30cluster1 -d @$1-hostmapping.json
